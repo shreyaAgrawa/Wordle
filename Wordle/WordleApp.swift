e4ca73884fa232b7648fbdefc3562fa4a7659422
shreyaAgrawa <102440427+shreyaAgrawa@users.noreply.github.com>
2024-08-07 15:38:17 -0500

    Initial commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Shreya Agrawal on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
